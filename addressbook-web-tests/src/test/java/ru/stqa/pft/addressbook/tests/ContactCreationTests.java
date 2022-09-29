7bc33294778517a5f5ae4c70b4a8fc8d3bbdc67b
EugenMakarevich <evgeny.makarevich.2019@gmail.com>
2022-09-29 21:45:26 +0200

    Исправление падающего теста создания контактов при запуске тест сьюта
    Добавил переход на главную страницу

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,12 +3,14 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
+
 import java.util.Comparator;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
+    app.goTo().goToHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
     ContactData contact = new ContactData("Vasya", "Pupkin", "www leningrad", "1234567", "+48123456789", "vasyapupkin@gmail.com", "test1");
     app.getContactHelper().createContact(contact);
@@ -18,14 +20,6 @@ public class ContactCreationTests extends TestBase {
     Comparator<? super ContactData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
     before.sort(byId);
     after.sort(byId);
-    for (ContactData contacts : before) {
-      System.out.println(contact);
-    }
-    System.out.println("");
-    for (ContactData contacts : after) {
-      System.out.println(contact);
-    }
-
     Assert.assertEquals(before, after);
   }
 }
