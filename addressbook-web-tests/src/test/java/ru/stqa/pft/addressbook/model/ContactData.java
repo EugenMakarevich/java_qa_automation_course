25f91db14c364bdaee7f8e525f0c1c60b62b1a8f
EugenMakarevich <evgeny.makarevich.2019@gmail.com>
2022-10-08 23:22:16 +0200

    Лекция 5.11. Клеим строки: метод обратных проверок

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -7,10 +7,10 @@ public class ContactData {
   private String firstName;
   private String lastName;
   private String address;
-  private String homephone;
-  private String mobilephone;
-
-  private  String workphone;
+  private String homePhone;
+  private String mobilePhone;
+  private  String workPhone;
+  private String allPhones;
   private String email;
   private String group;
 
@@ -30,15 +30,19 @@ public class ContactData {
   }
 
   public String getHomePhone() {
-    return homephone;
+    return homePhone;
   }
 
   public String getMobilePhone() {
-    return mobilephone;
+    return mobilePhone;
   }
 
   public String getWorkPhone() {
-    return workphone;
+    return workPhone;
+  }
+
+  public String getAllPhones() {
+    return allPhones;
   }
 
   public String getEmail() {
@@ -69,18 +73,23 @@ public class ContactData {
     return this;
   }
 
-  public ContactData withHomePhone(String homephone) {
-    this.homephone = homephone;
+  public ContactData withHomePhone(String homePhone) {
+    this.homePhone = homePhone;
     return this;
   }
 
-  public ContactData withMobilePhone(String mobilephone) {
-    this.mobilephone = mobilephone;
+  public ContactData withMobilePhone(String mobilePhone) {
+    this.mobilePhone = mobilePhone;
     return this;
   }
 
-  public ContactData withWorkPhone(String workphone) {
-    this.workphone = workphone;
+  public ContactData withWorkPhone(String workPhone) {
+    this.workPhone = workPhone;
+    return this;
+  }
+
+  public ContactData withAllPhones(String allPhones) {
+    this.allPhones = allPhones;
     return this;
   }
 
