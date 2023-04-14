1b70a79210ce05d4927af106eddcc7e9483d22df
EugenMakarevich <evgeny.makarevich.2019@gmail.com>
2023-04-14 20:50:02 +0200

    Лекция 8.6. Получение почты на встроенный в тесты почтовый сервер part 2

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -5,16 +5,22 @@ import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.appmanager.HelperBase;
 
 public class RegistrationHelper extends HelperBase {
-  private WebDriver wd;
 
   public RegistrationHelper(ApplicationManager app) {
     super(app);
   }
 
   public void start(String username, String email) {
-    wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+    wd.get(app.getProperty("web.baseUrl") + "signup_page.php");
     type(By.name("username"), username);
     type(By.name("email"), email);
-    click(By.cssSelector("input[value='Sighup']"));
+    click(By.cssSelector("input[value='Signup']"));
+  }
+
+  public void finish(String confirmationLink, String password) {
+    wd.get(confirmationLink);
+    type(By.name("password"), password);
+    type(By.name("password_confirm"), password);
+    click(By.cssSelector("button[type=submit]"));
   }
 }
