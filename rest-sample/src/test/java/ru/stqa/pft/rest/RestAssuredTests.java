7e66a869b7881d0b44b740dc401ee531e6563ac9
EugenMakarevich <evgeny.makarevich.2019@gmail.com>
2023-04-18 17:06:50 +0200

    Лекция 9.5. RestAssured: использовать или нет?

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
@@ -4,19 +4,21 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
-import org.apache.http.client.fluent.Executor;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.message.BasicNameValuePair;
-import org.testng.Assert;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
 
-public class RestTests {
+public class RestAssuredTests {
+  @BeforeClass
+  public void init() {
+    RestAssured.authentication = RestAssured.basic("b31e382ca8445202e66b03aaf31508a3", "");
+  }
+
   @Test
   public void testCreateIssue() throws IOException {
     Set<Issue> oldIssues = getIssues();
@@ -24,26 +26,21 @@ public class RestTests {
     int issueId = createIssue(newIssue);
     Set<Issue> newIssues = getIssues();
     oldIssues.add(newIssue.withId(issueId));
-    //assertEquals(oldIssues, newIssues);
+    assertEquals(oldIssues, newIssues);
   }
 
   private Set<Issue> getIssues() throws IOException {
-    String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json"))
-            .returnContent().asString();
+    String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json").asString();
     JsonElement parsed = new JsonParser().parse(json);
     JsonElement issues = parsed.getAsJsonObject().get("issues");
     return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType()) ;
   }
 
-  private Executor getExecutor() {
-    return Executor.newInstance().auth("b31e382ca8445202e66b03aaf31508a3", "");
-  }
-
   private int createIssue(Issue newIssue) throws IOException {
-    String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json")
-                    .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
-                              new BasicNameValuePair("description", newIssue.getDescription())))
-                    .returnContent().asString();
+    String json = RestAssured.given()
+            .parameter("subject", newIssue.getSubject())
+            .parameter("description", newIssue.getDescription())
+            .post("https://bugify.stqa.ru/api/issues.json").asString();
     JsonElement parsed = new JsonParser().parse(json);
     return parsed.getAsJsonObject().get("issue_id").getAsInt();
   }
